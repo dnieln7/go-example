8e903c6eb12922949230c51e9a1261ad494052d7
Daniel <dnieln7@gmail.com>
2023-09-05 21:15:28 -0600

    added client

---
+++ ws-client/go.mod
@@ -0,0 +1,5 @@
+module github.com/dnieln7/go-examples/ws-client
+
+go 1.21.0
+
+require github.com/gorilla/websocket v1.5.0
---
+++ ws-client/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net/url"
+	"os"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	url := url.URL{Scheme: "ws", Host: ":4444", Path: "/server"}
+
+	// Establish connection
+	connection, response, err := websocket.DefaultDialer.Dial(url.String(), nil)
+
+	if err != nil {
+		log.Fatal("Connection error: ", err)
+	}
+
+	log.Println("Connection establised with response: ", response)
+
+	defer connection.Close()
+
+	// Receive messages
+	go func() {
+		for {
+			messageType, message, err := connection.ReadMessage()
+
+			if err != nil {
+				log.Println("Error reading message:", err)
+			} else {
+				log.Println("Message:", message, " with type: ", messageType)
+			}
+
+		}
+	}()
+
+	// Read from stdin and send through websocket
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		err = connection.WriteMessage(websocket.TextMessage, []byte(scanner.Text()))
+
+		if err != nil {
+			log.Println("Error writing message:", err)
+		} else {
+			log.Println("Message sent: ", scanner.Text())
+		}
+	}
+}
